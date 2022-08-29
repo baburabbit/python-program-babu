49c1381163cc354b2241f87954c31713b5f5b42c
Babu Nagarajan <112308559+baburabbit@users.noreply.github.com>
2022-08-29 13:48:11 +0530

    Update quadratic equation.py

--- quadratic equation.py
+++ quadratic equation.py
@@ -1,7 +1,7 @@
 import cmath
-a=1
-b=5
-c=6
+a=int(input("enter the number1:"))
+b=int(input("enter the number2:"))
+c=int(input("enter the number3:"))
 d=(b**2)-(4*a*c)
 sol1=(-b-cmath.sqrt(d))/(2*a)
 sol2=(-b+cmath.sqrt(d))/(2*a)
